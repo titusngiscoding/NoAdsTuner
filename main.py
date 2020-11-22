1fed09f71b59b3f9b33852c765f31c0094b739d1
Titus Ng <titusngiscoding@gmail.com>
2020-11-22 22:36:19 +0800

    22 nov final

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import scipy.io.wavfile as wav
 
 def processAudio():
     fs=44100
-    duration = 5  # seconds
-    myrecording = sd.rec(duration * fs, samplerate=fs, channels=2,dtype='float64')
+    duration = 0.01  # seconds
+    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1,dtype='float64')
     print ("Recording Audio")
     sd.wait()
     print ("Audio recording complete , Play Audio")
+    print(myrecording)
     sd.play(myrecording, fs)
     sd.wait()
-    print ("Play Audio Complete")   
+    print ("Play Audio Complete")
 
 
 def main():
