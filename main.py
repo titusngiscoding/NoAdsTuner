54e99ceb299c228320980efef7d1a3ad9c54bd08
Titus Ng <titusngiscoding@gmail.com>
2020-11-22 22:01:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import sounddevice as sd
+import numpy as np
+import scipy.io.wavfile as wav
+
 def processAudio():
-    
+    fs=44100
+    duration = 5  # seconds
+    myrecording = sd.rec(duration * fs, samplerate=fs, channels=2,dtype='float64')
+    print ("Recording Audio")
+    sd.wait()
+    print ("Audio recording complete , Play Audio")
+    sd.play(myrecording, fs)
+    sd.wait()
+    print ("Play Audio Complete")
+
 
 def main():
     print("Hello World!")
